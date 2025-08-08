3da76404368aa51d69d7b5c00ccce3b9198f1434
Suraj20 <bhosalesuraj2012@gmail.com>
2025-08-08 12:04:51 +0530

    requirements are added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Chat-With-Doctor-RAG-Application
+
+#### command to create Virtual env
+
+```bash
+conda create -n env_name python=3.10 -y
+```
+
+#### Activate env
+
+```bash
+conda activate env_name
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Chat-With-Doctor-RAG-Application',
+    version='0.1.0',
+    author='Suraj Bhosale',
+    author_email='bhosalesuraj2012@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
